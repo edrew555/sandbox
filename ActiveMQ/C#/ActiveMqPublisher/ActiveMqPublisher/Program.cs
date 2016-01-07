a90f5f2d0ff73c716a0850e3badd5d2741987015
Eric Drewniak <edrewniak@mitre.org>
2016-01-07 14:00:48 -0500

    adding C# ActiveMQ pub/sub clients

---
+++ ActiveMQ/C#/ActiveMqPublisher/ActiveMqPublisher/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Text;
+using TopicPublisher;
+
+namespace TopicPublisher
+{
+    public class Program
+    {
+        const string TOPIC_NAME = "MyTestTopic";
+        const string BROKER = "tcp://localhost:61616";
+
+        private SimpleTopicPublisher publisher;
+        private readonly StringBuilder builder = new StringBuilder();
+        private delegate void SetTextCallback(string text);
+
+
+
+        static void Main(string[] args)
+        {
+            try
+            {
+                SimpleTopicPublisher publisher = new SimpleTopicPublisher(TOPIC_NAME, BROKER);
+                publisher.SendMessage("hello world");
+                publisher.Dispose();
+
+                Console.WriteLine("Message Sent.");
+                Console.WriteLine("Press any key to exit...");
+                Console.ReadKey();
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.ToString());
+                Console.WriteLine("Press any key to exit...");
+                Console.ReadKey();
+            }
+        }
+
+
+
+      
+    }
+}
---
+++ ActiveMQ/C#/ActiveMqSubscriber/ActiveMQ/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using TopicSubscriber;
+
+namespace TopicSubscriber
+{
+    class Program
+    {
+        const string TOPIC_NAME = "MyTestTopic";
+        const string BROKER = "tcp://localhost:61616";
+        const string CLIENT_ID = "Subscriber1";
+        const string CONSUMER_ID = "Subscriber1";
+
+        static void Main(string[] args)
+        {
+            try
+            {
+                using (SimpleTopicSubscriber subscriber = new SimpleTopicSubscriber(TOPIC_NAME, BROKER, CLIENT_ID, CONSUMER_ID))
+                {
+                    subscriber.OnMessageReceived += new MessageReceivedDelegate(subscriber_OnMessageReceived);
+                    
+                    Console.WriteLine("Listening for Messages.");
+                    Console.WriteLine("Press any key to exit...");
+                    Console.ReadKey();
+
+                    subscriber.Dispose();
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex);
+                Console.WriteLine("Press any key to exit...");
+                Console.ReadKey();
+            }
+
+
+        }
+
+        static void subscriber_OnMessageReceived(string message)
+        {
+            Console.WriteLine(message);
+        }
+    }
+}
